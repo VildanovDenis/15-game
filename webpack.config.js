ff8057e07d8306ca48ae68ffe1ae1c768dea4118
Denis <disamapm@gmail.com>
2019-05-27 16:27:53 +0300

    Инициализирует проект

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ]
+};
