aff4b573eb08c83bf2878faf1727abff26f1650b
Denis <disamapm@gmail.com>
2019-05-30 10:54:52 +0300

    Добавляет кнопку начала новой игры, счетчик ходов

--- src/components/gameContainer/index.jsx
+++ src/components/gameContainer/index.jsx
@@ -13,35 +13,69 @@ import { initialCells } from '../../gameData/index.js';
 const StyledWrapper = styled.section`
     display: flex;
     justify-content: center;
-    align-items: center;
     height: 475px;
     position: relative;
     width: 475px;
     margin: 0 auto;
+`;
+const StyledHeaderWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 475px;
+    margin: 0 auto 15px;
+`;
+const StyledBtn = styled.button`
+    width: 200px;
+    height: 50px;
+    padding: 0;
+    border: none;
+    border-radius: 5px;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    line-height: 50px;
+    letter-spacing: 0.5px;
+    text-align: center;
+    background-color: rgba(238,228,218,0.9);
+    color: #6a4e4e;
 `;
 
 class GameContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            cells: []
+            cells: [],
+            steps : 0
         };
 
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.onNewGameButtonClick = this.onNewGameButtonClick.bind(this);
     }
 
 
     handleKeyPress(event) {
         const direction = setDirectionName(event.code);
+        const newSteps = this.state.steps + 1;
 
         if (direction !== '') {
             const { cells } = this.state;
             const newCells = moveCells(cells, direction);
 
             this.setState({
-                cells: newCells
+                cells: newCells,
+                steps: newSteps
             })
         };
+    }
+
+    onNewGameButtonClick() {
+        const newCells = randomCells(initialCells);
+
+        this.setState({
+            cells: newCells,
+            steps: 0
+        });
     }
 
     componentDidMount() {
@@ -59,13 +93,24 @@ class GameContainer extends React.Component {
     }
 
     render() {
-        const { cells } = this.state;
+        const { cells, steps } = this.state;
 
         return (
-            <StyledWrapper>
+            <React.Fragment>
+                <StyledHeaderWrapper>
+                    <StyledBtn
+                        type="button"
+                        onClick={this.onNewGameButtonClick}
+                    >
+                        Новая игра
+                    </StyledBtn>
+                    <StyledBtn as="div">Счет: {steps}</StyledBtn>
+                </StyledHeaderWrapper>
+                <StyledWrapper>
                 <GameBgComponents />
                 <GamePlaygroundComponent cells={cells}/>
-            </StyledWrapper>
+                </StyledWrapper>
+            </React.Fragment>
         )
     }
 }
