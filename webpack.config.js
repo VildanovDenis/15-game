62a37a824eb4eb363289523cbc4526102d91c04a
VildanovDenis <disamapm@gmail.com>
2019-06-01 13:54:38 +0300

    Создает сборку для прода

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/"
+    publicPath: "./"
   },
   plugins: [
     new HtmlWebpackPlugin({
